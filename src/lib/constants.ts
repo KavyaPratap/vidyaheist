7672c005e085cc49facfe494123a14fc4aaeb3a2
KavyaPratap Singh <kavyapratapsingh221@gmail.com>
2026-03-01 15:29:34 +0530

    Add files via upload

---
+++ src/lib/constants.ts
@@ -0,0 +1,9 @@
+
+import type { TestSeriesType, QuestionType } from "./types";
+
+export const APP_NAME = "Vidyaheist";
+
+// Mock data is no longer needed as we are migrating to a database.
+export const MOCK_TEST_SERIES: TestSeriesType[] = [];
+
+export const MOCK_QUESTIONS: QuestionType[] = [];
